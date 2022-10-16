5f6ec13a05a4c5678e8f2f724a958e38280b6f13
Liccob <tql9431@gmail.com>
2022-10-16 12:07:50 +0800

    feat: 完成部分

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(counts: i32) -> i32 {
+    if (counts > 40) {
+        return counts * 1;
+    } else {
+        return counts * 2;
+    }
+}
 
 // Don't modify this function!
 #[test]
